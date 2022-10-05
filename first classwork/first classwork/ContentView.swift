859b0faab6f9af02ac3a9822ee1ffccc1269c52c
RihamElshikh <115099803+RihamElshikh@users.noreply.github.com>
2022-10-05 19:45:33 +0300

    .

---
+++ first classwork/first classwork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first classwork
+//
+//  Created by mac on 05/10/2022.
+//  Copyright © 2022 mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
